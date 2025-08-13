afe2d6a1e7d97228af2b5d223e1427c24a729a0f
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2025-08-12 21:46:38 -0300

    Add files via upload

---
+++ condicional.js
@@ -0,0 +1,8 @@
+let numero1 = 2;
+let numero2 = 2;
+// a soma só acontecerá se o a variável numero1 for menor que a variável numero2
+if (numero1 < numero2) {
+    let resultado = numero1 + numero2;
+    console.log("A soma dos números é: ", resultado);
+    // a ordem dos fatores não altera o produto na soma
+}
