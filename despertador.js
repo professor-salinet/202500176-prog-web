5753ff457c851b210ddc9fd3dbcf5b1ff720a25d
Projects repositories of Professor Salinet (SENAC) <professorsalinet@gmail.com>
2025-09-29 20:27:50 -0300

    Add files via upload

---
+++ despertador.js
@@ -0,0 +1,54 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function despertador() {
+    console.log("Bem vindo ao despertador!");
+    readline.question("Digite a hora (de 0 a 23), depois tecle Enter: ", hora => {
+        readline.question("Digite o minuto (de 0 a 59), depois tecle Enter: ", minuto => {
+            programarDespertador(parseInt(hora), parseInt(minuto));
+        });
+    });
+}
+
+var data = new Date();
+var horaAtual = data.getHours();
+var minutoAtual = data.getMinutes();
+var segundoAtual = data.getSeconds();
+var intervalo;
+function programarDespertador(hora, minuto) {
+    try {
+        intervalo = setInterval(() => {
+            data = new Date();
+            horaAtual = data.getHours();
+            minutoAtual = data.getMinutes();
+            segundoAtual = data.getSeconds();
+            // console.clear();
+            console.log("Agora são: " + horaAtual + ":" + minutoAtual + ":" + segundoAtual);
+            console.log("O despertador está programado para notificar às: " + hora + ":" + minuto);
+            if (parseInt(horaAtual) >= hora) {
+                if (parseInt(minutoAtual) >= minuto) {
+                    console.log("O despertador está ativo.");
+                    perguntarNovamente();
+                }
+            }
+        },1000);
+    } catch (error) {
+        console.error("Algo de errado não está certo, veja o erro: ", error);
+    }
+}
+
+function perguntarNovamente() {
+    clearInterval(intervalo);
+    readline.question("Deseja programar um novo despertador? \n\r [ s ] - sim \n\r [ n ] - não \n\r > ", resposta => {
+        if (resposta.toLowerCase() === "s") {
+            despertador();
+        } else {
+            console.log("Obrigado por utilizar o despertador. Até a próxima...");
+            readline.close();
+        }
+    });
+}
+
+despertador();
