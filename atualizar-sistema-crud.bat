a1b25bfc48d605cfc1254682bcdf3bdb3ffc9b15
Projects repositories of Professor Salinet (SENAC) <professorsalinet@gmail.com>
2025-11-14 19:52:40 -0300

    Add files via upload

--- atualizar-sistema-crud.bat
+++ atualizar-sistema-crud.bat
@@ -1,30 +1,33 @@
-echo "Encerrando o node"
+echo "Encerrando o node\n"
 taskkill /F /IM node.exe
 
-echo "Acessando diretorio do usuario na maquina"
+echo "Acessando diretorio do usuario na maquina\n"
 cd %userprofile%
 cd Documents
 
-echo "Excluindo diretorio antigo"
+echo "Excluindo diretorio antigo\n"
 del /s /f /q 202500176-prog-web-30-09-25-sistema-crud
 
-echo "Fazendo download do repositorio"
+echo "Fazendo download do repositorio\n"
 curl -L -o sistema-crud.zip https://github.com/professor-salinet/202500176-prog-web/archive/refs/heads/30-09-25-sistema-crud.zip
 
-echo "Descompactando arquivo baixado"
+echo "Descompactando arquivo baixado\n"
 tar -xf sistema-crud.zip -C .
 
-echo "Acessando diretorio novo"
+echo "Acessando diretorio novo\n"
 cd 202500176-prog-web-30-09-25-sistema-crud
 
-echo "Instalando dependencias"
+echo "Instalando dependencias\n"
 call npm i -y
 
-echo "Executando vscode no diretório atualizado do sistema"
+echo "Executando vscode no diretorio atualizado do sistema\n"
 call code .
 
-echo "Executando node servidor.js"
-node servidor.js
+echo "Abrindo localhost no navegador\n"
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --incognito http://localhost
+
+echo "Executando node servidor.js\n"
+call node servidor.js
 
 echo "Fim da execucao"
 pause
